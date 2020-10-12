db98db78817af2d8a28385f1a1d8c6667af8b839
almarpr4 <almarpr4@server.fake>
2020-10-12 14:26:16 +0200

    first name shown in Appointment

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -11,6 +11,17 @@ describe('Appointment', () => {
 
     ReactDOM.render(component, container);
 
-    expect(document.body.textContent).toMatch('Ashley');
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('renders the customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container)
+
+    ReactDOM.render(component, container);
+
+    expect(container.textContent).toMatch('Jordan');
   });
 });
