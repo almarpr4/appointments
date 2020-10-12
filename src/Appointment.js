b2de48ba7aeb5f65fa0c5b1b91b3ae72111bd451
almarpr4 <almarpr4@server.fake>
2020-10-12 18:46:25 +0200

    Show date on appointments

--- src/Appointment.js
+++ src/Appointment.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 export const Appointment = ({customer}) => ( < div > {customer.firstName} < /div>);
 
-export const AppointmentsDayView = ({appointments}) => (<div id="appointmentsDayView">
+export const AppointmentsDayView = ({appointments}) => (
+  <div id="appointmentsDayView">
   <ol>
   {appointments.map((appointment) => (
-      <div key={appointment.startsAt} />
+      <div key={appointment.startsAt}>
+      <li>{appointmentTimeOfDay(appointment.startsAt)}</li>
+    </div>
     ))}
   </ol>
-  </div>);
+  </div>
+);
+
+const appointmentTimeOfDay = startsAt => {
+    const [h, m] = new Date(startsAt).toTimeString().split(':');
+    return `${h}:${m}`;
+}
